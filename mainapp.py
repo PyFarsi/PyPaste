b8b8ccf9baa93b0f5b56312c2913a748e0659431
Serqati <59086060+Serqati@users.noreply.github.com>
2021-04-20 01:26:21 -0700

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -40,4 +40,4 @@ def view(key):
     return render_template('viewer.html', row=False, code=get_content(key), extension=ext)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=80)
